174e5c1bee808c7e64d920600fe05a614a9b1707
yasinsabri <yasinsabri869@gmail.com>
2023-11-10 21:13:25 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import Navbar from "./component/layout/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
---
+++ src/component/layout/Navbar.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-expand-lg mx-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src="asset/download (3).svg" alt="" className="logo" />
+          </a>
+          {/* <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button> */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-4">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  <img src="asset/download (4).svg" alt="" />
+                </a>
+              </li>
+              <li className="nav-item mx-4">
+                <a className="nav-link" href="/">
+                  <img src="asset/download (5).svg" alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <nav className="navbar navbar-expand-lg mx-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img
+              src="asset/download (3).svg"
+              alt=""
+              className="logo"
+              width="70"
+            />
+          </a>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-4">
+              <form className="d-flex" role="search">
+                <input
+                  className="form-control nav-search me-2"
+                  type="search"
+                  placeholder="PAKISTAN"
+                  aria-label="Search"
+                />
+              </form>
+              <form className="d-flex" role="search">
+                {/* <input
+                  className="form-control nav-search1 me-2"
+                  type="search"
+                  placeholder="Find Cars, Mobile Phone and more..."
+                  aria-label="Search"
+                /> */}
+                <div className="input-group ">
+                  <input
+                    type="text"
+                    className="form-control nav-search1"
+                    placeholder="Find Cars, Mobile Phone and more..."
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                  />
+                  <span className="input-group-text nav-search1-end" id="basic-addon2">
+                    
+                  </span>
+                </div>
+              </form>
+              <li className="nav-item mx-3">
+                <a className="nav-link login-text" href="/">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item mx-1">
+                <a className="nav-link " href="/">
+                  <img src="asset/color.svg" alt="" className="w-100" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
